c279017eb23473f5bcc1b8ec8b2a0c30e02686e4
“anar <sehar.uzair23@gmail.com>
2023-06-14 19:03:44 -0300

    query and mutations defined

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,16 +1,18 @@
 const { User } = require('../models');
 const {AuthenticationError} = require('apollo-server-express')//or should it be apollo-server-errors
 const {signToken} = require('../utils/auth')
+
 const resolvers = {
   Query: {
     me: async (parent, args, context) => {
       if (context.user) {
-        return User.findOne({ _id: context.user._id });
+       const userData = User.findOne({ _id: context.user._id }).select('-__v -password');
+        return userData;
       }
       throw new AuthenticationError('You need to be logged in!');
     },
-
   },
+
   Mutation: {
     signup: async (parent, args) => {
       const user = await User.create(args);
@@ -18,6 +20,44 @@ const resolvers = {
       return {user, token};
     },
 
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw new AuthenticationError('User not found. Do you have an account?');
+      }
+      const correctPw = await user.isCorrectPassword(password);
+      
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials!');
+      }
+      const token = signToken(user);
+      
+      return { token, user };
+    },
+
+    saveBook: async (parent, { newBook }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $push: { savedBooks: newBook }},
+          { new: true }
+        );
+        return updatedUser;
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+
+    removeBook: async (parent, { bookId }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId }}},
+          { new: true }
+        );
+        return updatedUser;
+      }
+      throw new AuthenticationError('Login required!');
+    },
   },
 };
 
